8899fa02ffae6c8604aaf544bee53ec0be516bfa
Duy Nguyen <Kduy9180@gmail.com>
2024-02-05 20:23:07 +0700

    make login.php look better

--- login.php
+++ login.php
@@ -6,22 +6,21 @@
                 <div class="login-title">
                     Đăng nhập
                 </div>
-                <div class="username">
-                    <form action="" method="get">
+    
+                <form action="" method="post" onsubmit="setisLoginTrue(this)">
+                    <div class="username">
+                            <input type="text" id="username" name="username" placeholder="Email/Số điện thoại/Tên đăng nhập">
+                    </div>
+                    <div class="username">               
                         <input type="text" id="username" name="username" placeholder="Email/Số điện thoại/Tên đăng nhập">
-                    </form>
-                </div>
-                <div class="password" >
-                    <form action="" method="get">
-                        <input type="password" id="password" name="password" placeholder="Mật khẩu">
-                    </form>
-                </div>
-
-                <div class="submit">
-                    <form action="" method="get" onsubmit="setisLoginTrue(this)" target="_blank">
-                        <input type="submit" id="submit" name="submit" value="Đăng nhập" >
-                    </form>
-                </div>
+                    </div>
+                    <div class="password" >
+                            <input type="password" id="password" name="password" placeholder="Mật khẩu">
+                    </div>
+                    <div class="submit">
+                            <input type="submit" id="submit" name="submit" value="Đăng nhập" >
+                    </div>
+                </form>
 
             </div>
 
