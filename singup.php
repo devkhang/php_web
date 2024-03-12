99288f82bc6211feeb221eaf339efb17e2095b14
Duy Nguyen <Kduy9180@gmail.com>
2024-03-12 12:16:16 +0700

    nothing

--- singup.php
+++ singup.php
@@ -1,6 +1,63 @@
-<?php
-    include_once("header.php")
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="css\layout_style.css">
+
+    <style>
+        #main_content{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        .box-signup{
+            width: 50%;
+            height: 250px;
+            text-align: center;
+
+            background-color: rgba(255,255,255,255);
+            border-style: solid;
+            border-width: 1px;
+
+            box-sizing: border-box;
+
+            
+        }
+        .box-signup #singup-title{
+            font-size: 20px;
+            font-weight:bolder;
+            margin-bottom: 30px;
+
+            
+        }
+        .box-signup div{
+            margin-bottom: 20px;
+            width: 100%;
+        }
+        .box-signup .box-itme form input{
+            width: 95%;
+            height: 30px;
+        }
+       
+        .box-signup #submit form input{
+       
+            width: 100%;
+            height: 30px;
+            background-color: rgba(243,130,108,255);
+            cursor: pointer;
+        }
+    </style>
+
+
+</head>
+<body>
+    <?php include_once("headerRegion.php"); ?>
+    <div id="main_body">
+        <?php include_once("leftPannelRegion.php"); ?>
+
         <div id="main_content1">
             <div class="box-signup">
                 <div class="box-itme" id="singup-title">
@@ -41,9 +98,15 @@
                     ?>
             </div>
         </div>
-        <div id="right_panel">
-        </div>
-    </div>
-<?php
-    include_once("footer.php")
-?>
+        
+        <?php include_once("rightPannelRegion.php"); ?>
+    </div>    
+
+    <?php include_once("footerRegion.php"); ?>
+
+
+</body>
+</html>
+
+
+        
