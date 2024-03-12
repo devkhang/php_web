0330141621c1559db6190fc1ff83a7dc6a3e4e65
Duy Nguyen <Kduy9180@gmail.com>
2024-03-12 12:23:05 +0700

    nothing

--- singup.php
+++ singup.php
@@ -8,47 +8,65 @@
     <link rel="stylesheet" href="css\layout_style.css">
 
     <style>
-        #main_content{
-            display: flex;
-            justify-content: center;
-            align-items: center;
-        }
-        .box-signup{
-            width: 50%;
-            height: 250px;
-            text-align: center;
+       .box-signup{
+        width: 50%;
+        text-align: center;
 
-            background-color: rgba(255,255,255,255);
-            border-style: solid;
-            border-width: 1px;
+        background-color: rgba(255,255,255,255);
+        border-style: solid;
+        border-width: 1px;
 
-            box-sizing: border-box;
+        box-sizing: border-box;
 
-            
-        }
-        .box-signup #singup-title{
-            font-size: 20px;
-            font-weight:bolder;
-            margin-bottom: 30px;
+        
+    }
+    .box-signup #singup-title{
+        font-size: 20px;
+        font-weight:bolder;
+        margin-bottom: 30px;
 
-            
-        }
-        .box-signup div{
-            margin-bottom: 20px;
-            width: 100%;
-        }
-        .box-signup .box-itme form input{
-            width: 95%;
-            height: 30px;
-        }
-       
-        .box-signup #submit form input{
-       
-            width: 100%;
-            height: 30px;
-            background-color: rgba(243,130,108,255);
-            cursor: pointer;
-        }
+        
+    }
+    .box-signup div{
+        margin-bottom: 20px;
+        width: 100%;
+    }
+    .box-signup .box-itme form input{
+        width: 95%;
+        height: 30px;
+        margin: 3px;
+    }
+
+    /* .box-signup #submit form input{
+
+        width: 100%;
+        height: 30px;
+        background-color: rgba(243,130,108,255);
+        cursor: pointer;
+    } */
+    #div_form_signup{
+        margin:0px;
+        height:auto;
+    }
+    .box-signup .box-itme form #submit{
+        margin-bottom:auto;
+        height:auto;
+    }
+    #form_signup{
+        height:auto;
+    }
+    .box-signup .box-itme form #submit-input{
+        width: 100%;
+        box-sizing:border-box;
+        height: 30px;
+        background-color: rgba(243,130,108,255);
+        cursor: pointer;
+        margin:0px;
+        margin-bottom:auto;
+        border-style:none;
+    }
+
+
     </style>
 
 
