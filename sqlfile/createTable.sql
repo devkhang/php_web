666698ee398ecc30e3cf44488dd115c03c2439de
Duy Nguyen <Kduy9180@gmail.com>
2024-03-13 21:15:11 +0700

    create createTable.sql

---
+++ sqlfile/createTable.sql
@@ -0,0 +1,10 @@
+CREATE TABLE SANPHAM(
+    HangDTH varchar(10),
+    Ten varchar(50),
+    Mau varchar(10),
+    KhuyeMai int,
+    MieuTa text,
+    SoLuongTonKho int NOT NULL,
+    Gia int,
+    HinhAnhMH varchar(100)
+);
