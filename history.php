1636b82e4d334d9e7cdcf5946ac38f14851e10d5
Duy Nguyen <Kduy9180@gmail.com>
2024-03-13 22:05:42 +0700

    history.php

--- history.php
+++ history.php
@@ -1,7 +1,39 @@
-<?php
-    include_once("header.php")
-?>
-    <div id="main_content">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/layout_style.css">
+
+    <style>
+        #Order-history{
+            background-color: aliceblue;
+        }
+        #Order-history .history-box{
+            background-color: rgba(238,238,238,255);
+            border-style: solid;
+            border-width: 1px;
+            margin-bottom: 10px;
+        }
+        #Order-history .history-box:hover{
+            background-color:rgba(29,162,216);
+            border-style: solid;
+            border-width: 1px;
+            margin-bottom: 10px;
+            cursor: pointer;
+        }
+    </style>
+
+
+</head>
+<body>
+    <?php include_once("headerRegion.php"); ?>
+    <div id="main_body">
+
+        <?php include_once("leftPannelRegion.php"); ?>
+
+        <div id="main_content">
             
             <div id="Order-history">
                 <div class="history-box" id="order1" onclick="window.location.href = 'order1.html';">
@@ -23,11 +55,16 @@
                
                 
             </div>
+            
+        </div>
 
-        </div>
-        <div id="right_panel">
-        </div>
+        <?php include_once("rightPannelRegion.php"); ?>
+        
     </div>
-<?php
-    include_once("footer.php")
-?>
+
+    <?php include_once("footerRegion.php"); ?>
+
+    
+    
+</body>
+</html>
