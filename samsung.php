0ef98ab478de0d51cd9945b68f2f47fb1f1255a8
Duy Nguyen <Kduy9180@gmail.com>
2024-03-14 09:04:47 +0700

    transform samsung.html to php

---
+++ samsung.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="css\layout_style.css">
+    <link rel="stylesheet" href="css\product_list_layout_style.css">
+
+
+</head>
+<body>
+    <?php include_once("headerRegion.php"); ?>
+    <div id="main_body">
+        <?php include_once("leftPannelRegion.php"); ?>
+
+        <div id="main_content">
+            <ul id="phone_list" class="main_content_element">
+                <!-- start here -->
+                
+
+                <li class="phone_list_element">
+                    <a href="samsung-galaxy-z-fold5-kem.html">
+                        <div class="phone_list_element_img">
+                            <img alt="thêm ảnh" src="assets/samsung-galaxy-z-fold5- kem-600x600.jpg">
+                        </div>
+        
+                        <h3>
+                            Samsung Galaxy Z Fold5 5G
+                        </h3>
+                        
+                        <div class="phone_list_element_compare">
+                                    <span>Chính 7.6" &amp; Phụ 6.2"</span>
+                                    <span>Quad HD+ (2K+)</span>
+                        </div>
+
+                        <strong class="phone_list_element_price">40.990.000₫</strong>
+            
+            
+            
+                        <div class="phone_list_element_rating">
+                            <p>
+                                    <span><img class="icon-star" src="assets/star.png"></span>
+                                    <span><img class="icon-star" src="assets/star.png"></span>
+                                    <span><img class="icon-star" src="assets/star.png"></span>
+                                    <span><img class="icon-star" src="assets/star.png"></span>
+                                    <span><img class="icon-star" src="assets/bad-icon-starjpg.jpg"></span>
+                                    
+                            </p>
+                            <p class="item-rating-total">5</p>
+                        </div>   
+                    </a>
+                </li>
+
+                <li class="phone_list_element">
+                    <a href="Galaxy-S22-Ultra-Burgundy.html">
+                        <div class="phone_list_element_img">
+                            <img alt="Samsung Galaxy S22 Ultra 5G 128GB" src="assets/Galaxy-S22-Ultra-Burgundy-600x600.jpg">
+                        </div>
+        
+                        <h3>
+                            Samsung Galaxy S22 Ultra 5G 128GB
+                        </h3>
+                        
+                        <div class="phone_list_element_compare">
+                                    <span>6.8"</span>
+                                    <span>Quad HD+ (2K+)</span>
+                        </div>
+
+                        <strong class="phone_list_element_price">16.990.000₫</strong>
+            
+            
+            
+                        <div class="phone_list_element_rating">
+                            <p>
+                                    <span><img class="icon-star" src="assets/star.png"></span>
+                                    <span><img class="icon-star" src="assets/star.png"></span>
+                                    <span><img class="icon-star" src="assets/star.png"></span>
+                                    <span><img class="icon-star" src="assets/star.png"></span>
+                                    <span><img class="icon-star" src="assets/bad-icon-starjpg.jpg"></span>
+                                    
+                            </p>
+                            <p class="item-rating-total">194</p>
+                        </div>   
+                    </a>
+                </li>
+
+                <li class="phone_list_element">
+                    <a href="samsung-galaxy-z-fold5-kem.html">
+                        <div class="phone_list_element_img">
+                            <img alt="thêm ảnh" src="assets/samsung-galaxy-z-fold5- kem-600x600.jpg">
+                        </div>
+        
+                        <h3>
+                            Samsung Galaxy Z Fold5 5G
+                        </h3>
+                        
+                        <div class="phone_list_element_compare">
+                                    <span>Chính 7.6" &amp; Phụ 6.2"</span>
+                                    <span>Quad HD+ (2K+)</span>
+                        </div>
+
+                        <strong class="phone_list_element_price">40.990.000₫</strong>
+            
+            
+            
+                        <div class="phone_list_element_rating">
+                            <p>
+                                    <span><img class="icon-star" src="assets/star.png"></span>
+                                    <span><img class="icon-star" src="assets/star.png"></span>
+                                    <span><img class="icon-star" src="assets/star.png"></span>
+                                    <span><img class="icon-star" src="assets/star.png"></span>
+                                    <span><img class="icon-star" src="assets/bad-icon-starjpg.jpg"></span>
+                                    
+                            </p>
+                            <p class="item-rating-total">5</p>
+                        </div>   
+                    </a>
+                </li>
+
+                <li class="phone_list_element">
+                    <a href="Galaxy-S22-Ultra-Burgundy.html">
+                        <div class="phone_list_element_img">
+                            <img alt="Samsung Galaxy S22 Ultra 5G 128GB" src="assets/Galaxy-S22-Ultra-Burgundy-600x600.jpg">
+                        </div>
+        
+                        <h3>
+                            Samsung Galaxy S22 Ultra 5G 128GB
+                        </h3>
+                        
+                        <div class="phone_list_element_compare">
+                                    <span>6.8"</span>
+                                    <span>Quad HD+ (2K+)</span>
+                        </div>
+
+                        <strong class="phone_list_element_price">16.990.000₫</strong>
+            
+            
+            
+                        <div class="phone_list_element_rating">
+                            <p>
+                                    <span><img class="icon-star" src="assets/star.png"></span>
+                                    <span><img class="icon-star" src="assets/star.png"></span>
+                                    <span><img class="icon-star" src="assets/star.png"></span>
+                                    <span><img class="icon-star" src="assets/star.png"></span>
+                                    <span><img class="icon-star" src="assets/bad-icon-starjpg.jpg"></span>
+                                    
+                            </p>
+                            <p class="item-rating-total">194</p>
+                        </div>   
+                    </a>
+                </li>
+                <li class="phone_list_element">
+                    <a href="samsung-galaxy-z-fold5-kem.html">
+                        <div class="phone_list_element_img">
+                            <img alt="thêm ảnh" src="assets/samsung-galaxy-z-fold5- kem-600x600.jpg">
+                        </div>
+        
+                        <h3>
+                            Samsung Galaxy Z Fold5 5G
+                        </h3>
+                        
+                        <div class="phone_list_element_compare">
+                                    <span>Chính 7.6" &amp; Phụ 6.2"</span>
+                                    <span>Quad HD+ (2K+)</span>
+                        </div>
+
+                        <strong class="phone_list_element_price">40.990.000₫</strong>
+            
+            
+            
+                        <div class="phone_list_element_rating">
+                            <p>
+                                    <span><img class="icon-star" src="assets/star.png"></span>
+                                    <span><img class="icon-star" src="assets/star.png"></span>
+                                    <span><img class="icon-star" src="assets/star.png"></span>
+                                    <span><img class="icon-star" src="assets/star.png"></span>
+                                    <span><img class="icon-star" src="assets/bad-icon-starjpg.jpg"></span>
+                                    
+                            </p>
+                            <p class="item-rating-total">5</p>
+                        </div>   
+                    </a>
+                </li>
+
+                <li class="phone_list_element">
+                    <a href="Galaxy-S22-Ultra-Burgundy.html">
+                        <div class="phone_list_element_img">
+                            <img alt="Samsung Galaxy S22 Ultra 5G 128GB" src="assets/Galaxy-S22-Ultra-Burgundy-600x600.jpg">
+                        </div>
+        
+                        <h3>
+                            Samsung Galaxy S22 Ultra 5G 128GB
+                        </h3>
+                        
+                        <div class="phone_list_element_compare">
+                                    <span>6.8"</span>
+                                    <span>Quad HD+ (2K+)</span>
+                        </div>
+
+                        <strong class="phone_list_element_price">16.990.000₫</strong>
+            
+            
+            
+                        <div class="phone_list_element_rating">
+                            <p>
+                                    <span><img class="icon-star" src="assets/star.png"></span>
+                                    <span><img class="icon-star" src="assets/star.png"></span>
+                                    <span><img class="icon-star" src="assets/star.png"></span>
+                                    <span><img class="icon-star" src="assets/star.png"></span>
+                                    <span><img class="icon-star" src="assets/bad-icon-starjpg.jpg"></span>
+                                    
+                            </p>
+                            <p class="item-rating-total">194</p>
+                        </div>   
+                    </a>
+                </li>
+            </ul>
+
+            <div id="page_numbering">
+                <a href="samsung.html">1</a>
+                <a href="samsung2.html">2</a>
+            </div>
+        </div>
+
+
+        <?php include_once("rightPannelRegion.php"); ?>
+
+    </div>    
+
+    <?php include_once("footerRegion.php"); ?>
+
+
+</body>
+</html>
