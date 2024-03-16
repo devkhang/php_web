3bf71b19688c181a6334830f17ecd4698bda5b54
Duy Nguyen <Kduy9180@gmail.com>
2024-03-16 09:52:17 +0700

    update signup.php

--- singup.php
+++ singup.php
@@ -6,76 +6,35 @@
     <title>Document</title>
 
     <link rel="stylesheet" href="css\layout_style.css">
+    <link rel="stylesheet" href="css\modify_layout.css">
 
     <style>
-        #main_content1{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        background-image: url("assets/login_bg.jpg");
-        width: 100%;
-    }
-       .box-signup{
-        width: 50%;
-        text-align: center;
-
-        background-color: rgba(255,255,255,255);
-        border-style: solid;
-        border-width: 1px;
-
-        box-sizing: border-box;
-
+        #avatarFrom{
+            box-sizing: border-box;
+            height: 100%;
+            width: 100%;
+        }
+        #avatarFrom #avatar-real-value{
+            box-sizing: border-box;
+            height: 90%;
+            width: 100%;
+            background-color:chartreuse;
+        }
+        #avatarFrom #avatar-real-value:hover{
+            box-sizing: border-box;
+            height: 90%;
+            width: 100%;
+            background-color:aqua;
+            cursor: pointer;
+        }
         
-    }
-    .box-signup #singup-title{
-        font-size: 20px;
-        font-weight:bolder;
-        margin-bottom: 30px;
-
-        
-    }
-    .box-signup div{
-        margin-bottom: 20px;
-        width: 100%;
-    }
-    .box-signup .box-itme form input{
-        width: 95%;
-        height: 30px;
-        margin: 3px;
-    }
-
-    /* .box-signup #submit form input{
-
-        width: 100%;
-        height: 30px;
-        background-color: rgba(243,130,108,255);
-        cursor: pointer;
-    } */
-    #div_form_signup{
-        margin:0px;
-        height:auto;
-    }
-    .box-signup .box-itme form #submit{
-        margin-bottom:auto;
-        height:auto;
-    }
-    #form_signup{
-        height:auto;
-    }
-    .box-signup .box-itme form #submit-input{
-        width: 100%;
-        box-sizing:border-box;
-        height: 30px;
-        background-color: rgba(243,130,108,255);
-        cursor: pointer;
-        margin:0px;
-        margin-bottom:auto;
-        border-style:none;
-    }
-
-
+        #right_panel #add-all-button{
+            background-color: green;
+        }
+        #right_panel #add-all-button:hover{
+            background-color: red;
+        }
     </style>
-
 
 </head>
 <body>
@@ -83,45 +42,67 @@
     <div id="main_body">
         <?php include_once("leftPannelRegion.php"); ?>
 
-        <div id="main_content1">
-            <div class="box-signup">
-                <div class="box-itme" id="singup-title">
-                    Đăng kí
+        <div id="main_content">
+            <div id="user-info_box">
+                <div class="user-info-box-element" id="genernal-info">
+                    <div id="avatar">
+                        <form id="avatarFrom">
+                            <label for="avatar-real-value">Chọn avatar</label>
+                            <input id="avatar-real-value" name="avatar-real-value" type="file">
+                        </form>
+                    </div>
+                    
                 </div>
-                <div class="box-itme" id="div_form_signup">
-                    <form action="include/signup.inc.php" method="post" id="form_signup">
-                        <input type="text" id="username" name="username" placeholder="Email/Số điện thoại/Tên đăng nhập">
-                        <div class="box-itme" id="password">
-                            <input type="password" id="password" name="password" placeholder="Mật khẩu">
+                <div class="user-info-box-element" id="detail-info">
+                    <form id="detail-info-form">
+                        <div id="Account">
+                            Account
                         </div>
-                        <div class="box-itme" id="repassword">
-                            <input type="password" id="password1" name="rep_password" placeholder="Nhập lại mật khẩu">
+
+                        <div id="UserName-title">
+                            User name
                         </div>
-                        <div class="box-itme" id="submit">
-                            <input type="submit" id="submit-input" name="submit" value="Đăng ký">
+                        <div id="UserName" class="real-value">
+                            <input id="UserName" name="UserName" type="text" value="">
                         </div>
+
+                        <div id="Email-title">
+                            Email
+                        </div>
+                        <div id="Email" class="real-value">
+                            <input id="Email" name="Email" type="text" value="">
+                           
+                        </div>
+                        
+                        <div id="Adress-title">
+                            Địa chỉ
+                        </div>
+                        <div id="Adress" class="real-value">
+                            <input id="Adress" name="Adress" type="text" value="">
+                           
+                        </div>
+
+                        <div id="Pwd-title">
+                            Mật khẩu
+                        </div>
+                        <div id="Pwd" class="real-value">
+                            <input id="Adress" name="Adress" type="password" value="">
+                           
+                        </div>
+
+                        <div id="Phone-title">
+                            Số điện thoại
+                        </div>
+                        <div id="Phone" class="real-value">
+                            <input id="Phone" name="Phone" type="text" value="">
+                            
+                        </div>
+                    
                     </form>
                 </div>
-                <?php
-                        if(isset($_GET["error"])){
-                            if($_GET["error"]=="emptyInput"){
-                                echo "<p style='color:red'> fill all the field !</P>";
-                            }
-                            else if($_GET["error"]=="invalid_username"){
-                                echo "<p 'color:red'> choose the proper username !</P>";
-                            }
-                            else if($_GET["error"]=="password do not matching"){
-                                echo "<p 'color:red'> the repeat paword does not matching !</P>";
-                            }
-                            else if($_GET["error"]=="email exist"){
-                                echo "<p 'color:red'>Choose another username !</P>";
-                            }
-                            else if($_GET["error"]=="stmtfail"){
-                                echo "<p 'color:red'>something went wrong! try again</P>";
-                            }
-                        }
-                    ?>
             </div>
+
+            
         </div>
         
         <?php include_once("rightPannelRegion.php"); ?>
