a82b957cdf6400f1a6fb5e5e3f537d3a1efd0a21
Duy Nguyen <Kduy9180@gmail.com>
2024-04-05 23:26:15 +0700

    finish signup.php

--- signup.php
+++ signup.php
@@ -7,11 +7,8 @@ if(isset($_POST['dangky'])){
     //lay anh tu <input> va chuyen anh ve folder upload
     $userFile=$_FILES["avatar-real-value"];
     $uploadDir="./upload/". basename($userFile['name']);
-    if(move_uploaded_file($userFile["tmp_name"], $uploadDir)){
-        echo "upload succeed";
-    }
-    else{
-        echo "fail";
+    if(!move_uploaded_file($userFile["tmp_name"], $uploadDir)){
+        $err_moveFile=true;
     }
 
     //insert cac gia tri attribute cua taikhoan vao table taikhoan
@@ -22,10 +19,20 @@ if(isset($_POST['dangky'])){
     $userPWd=$_POST["pwd"];
     $userPhone=$_POST["Phone"];
 
-    $conn=connectDB();
-    $insertStatus=$conn->query(sprintf("INSERT INTO taikhoan (user_account, HinhDaiDien, DiaChi, Email, pwd, SoDTH) 
-    values
-    ('%s','%s','%s','%s','%s','%s')",$userName, $userAvatar, $userAddress, $userEmail, $userPWd, $userPhone));
+    if($userAvatar&& $userName && $userEmail && $userAddress && $userAddress
+    && $userPWd && $userPhone){
+        $conn=connectDB();
+        $insertStatus=$conn->query(sprintf("INSERT INTO taikhoan (user_account, HinhDaiDien, DiaChi, Email, pwd, SoDTH) 
+        values
+        ('%s','%s','%s','%s','%s','%s')",$userName, $userAvatar, $userAddress, $userEmail, $userPWd, $userPhone));
+        $err_singup=true;
+    }
+    else{
+        $err_emptyValue=true;
+    }
+    
+
+    
     
 
 
@@ -47,8 +54,15 @@ if(isset($_POST['dangky'])){
 
     <link rel="stylesheet" href="css\layout_style.css">
     <link rel="stylesheet" href="css\modify_layout.css">
+    <?php 
+        include_once('./include/commonStyles.php');
+        include_once('./include/commonScript.php')
+    ?>
 
     <style>
+        #detail-info{
+            margin-top: 10px;
+        }
         #avatarFrom{
             box-sizing: border-box;
             height: 100%;
@@ -85,13 +99,39 @@ if(isset($_POST['dangky'])){
         <div id="main_content">
             <div id="user-info_box">
                 <form  action="signup.php" name="signupForm" method="POST" enctype="multipart/form-data">
-                
-                
+                    <?php
+                        if(isset($err_moveFile)){?>
+                            <div class="alert alert-danger alert-dismissible">
+                                
+                                <strong>Thất bại!</strong> Đăng ký thất bại do không upload được file.
+                                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                            </div>
+                    <?php
+                        }
+                        else if(isset($err_emptyValue)){?>
+                            <div class="alert alert-danger alert-dismissible">
+                                
+                                <strong>Thất bại!</strong>Do thiếu thông tin nhập.
+                                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                            </div>
+                    <?php
+                        } else if(isset($err_singup)){?>
+                            <div class="alert alert-success alert-dismissible">
+                                
+                                <strong>Thành công!</strong>Bạn đã đăng ký thành công.
+                                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                            </div>
+                    <?php
+                        }
+                    ?>
+                    
+
+
                     <div class="user-info-box-element" id="genernal-info">
                         <div id="avatar">
                             <div id="avatarFrom">
                                 <label for="avatar-real-value">Chọn avatar</label>
-                                <input id="avatar-real-value" name="avatar-real-value" type="file">
+                                <input id="avatar-real-value" name="avatar-real-value" type="file" required>
                             </div>
                         </div>
                 
@@ -105,31 +145,31 @@ if(isset($_POST['dangky'])){
                                 User name
                             </div>
                             <div id="UserName" class="real-value">
-                                <input id="UserName" name="UserName" type="text" value="">
+                                <input id="UserName" name="UserName" type="text" value="" required>
                             </div>
                             <div id="Email-title">
                                 Email
                             </div>
                             <div id="Email" class="real-value">
-                                <input id="Email" name="Email" type="text" value="">
+                                <input id="Email" name="Email" type="text" value="" required>
                             </div>
                             <div id="Adress-title">
                                 Địa chỉ
                             </div>
                             <div id="Adress" class="real-value">
-                                <input id="Adress" name="Adress" type="text" value="">
+                                <input id="Adress" name="Adress" type="text" value="" required>
                             </div>
                             <div id="Pwd-title">
                                 Mật khẩu
                             </div>
                             <div id="Pwd" class="real-value">
-                                <input id="Adress" name="pwd" type="password" value="">
+                                <input id="Pwd" name="pwd" type="password" value="" required>
                             </div>
                             <div id="Phone-title">
                                 Số điện thoại
                             </div>
                             <div id="Phone" class="real-value">
-                                <input id="Phone" name="Phone" type="text" value="">
+                                <input id="Phone" name="Phone" type="text" value="" required>
                             </div>
                         </div>
                     </div>
